386862da95a19150d85501a40be044515718f6e9
Aliaksandr Rahalevich <saksmlz@gmail.com>
2018-01-26 22:23:34 -0800

    add url params

--- src/models/dump.rs
+++ src/models/dump.rs
@@ -1,13 +1,16 @@
-use rocket::request::{self, FromRequest, Request};
-use rocket::Outcome;
-use std::collections::HashMap;
 extern crate time;
+extern crate url;
+
+use rocket::request::Request;
+use std::collections::HashMap;
+use self::url::Url;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Dump {
     pub method: String,
     pub uri: String,
     pub headers: HashMap<String, String>,
+    pub url_params: HashMap<String, String>,
     pub body: Option<String>,
     pub time: String,
 }
@@ -15,36 +18,12 @@ pub struct Dump {
 fn time_str() -> String {
     let now = time::get_time();
     let utc = time::at_utc(now);
-    let tm = utc.rfc3339();
-    format!("{}", tm)
-}
 
-impl<'a, 'r> FromRequest<'a, 'r> for Dump {
-    type Error = ();
-
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Dump, ()> {
-        let uri = request.uri().as_str();
-        let mut headers = HashMap::new();
-
-        for header in request.headers().iter() {
-            headers
-                .insert(header.name().to_string(), header.value().to_string());
-        }
-
-        let dump = Dump {
-            method: request.method().to_string(),
-            uri: String::from(&uri[37..]), // cut first 37 chars out
-            headers: headers,
-            body: None,
-            time: time_str(),
-        };
-        return Outcome::Success(dump);
-    }
+    utc.rfc3339().to_string()
 }
 
 impl<'a, 'r> From<&'a Request<'r>> for Dump {
     fn from(request: &'a Request<'r>) -> Self {
-        let uri = request.uri().as_str();
         let mut headers = HashMap::new();
 
         for header in request.headers().iter() {
@@ -52,10 +31,19 @@ impl<'a, 'r> From<&'a Request<'r>> for Dump {
                 .insert(header.name().to_string(), header.value().to_string());
         }
 
+        let mut url_params = HashMap::new();
+        let uri = "http://abc.com/".to_string() + request.uri().as_str();
+        if let Ok(parsed_uri) = Url::parse(&uri) {
+            for (key, value) in parsed_uri.query_pairs() {
+                url_params.insert(key.into(), value.into());
+            }
+        }
+
         Self {
             method: request.method().to_string(),
-            uri: String::from(&uri[37..]), // cut first 37 chars out
+            uri: String::from(&request.uri().path()[37..]), // cut first 37 chars out
             headers: headers,
+            url_params: url_params,
             body: None,
             time: time_str(),
         }
